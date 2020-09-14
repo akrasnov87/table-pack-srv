3561b7a6059255e5c0d3cacdecd3b14ed67ad5bb
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-09-14 16:47:20 +0300

    update

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var vPath = process.env.virtualDirPath || '';
 var app = express();
 
 // view engine setup
@@ -15,10 +14,9 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(vPath + '/', express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(vPath + '/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
