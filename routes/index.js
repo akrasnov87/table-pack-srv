62a653d0222ce4d012e5c3520030c55371c09703
Aleksandr Krasnov <akrasnov87@gmail.com>
2020-09-15 15:32:59 +0300

    update docs

--- routes/index.js
+++ routes/index.js
@@ -19,21 +19,21 @@ var pkg = require('../package.json');
 router.get('/', async function(req, res, next) {
 
     var csvZip = join(__dirname, '../', 'public', 'csv-zip');
-    var csv = join(__dirname, '../', 'public', 'csv');
-    var json = join(__dirname, '../', 'public', 'json');
-    var jsonZip = join(__dirname, '../', 'public', 'json-zip');
+    //var csv = join(__dirname, '../', 'public', 'csv');
+    //var json = join(__dirname, '../', 'public', 'json');
+    //var jsonZip = join(__dirname, '../', 'public', 'json-zip');
 
-    var csvItems = await fsUtil.getTables(csv);
+    //var csvItems = await fsUtil.getTables(csv);
     var csvZipItems = await fsUtil.getTables(csvZip);
-    var jsonItems = await fsUtil.getTables(json);
-    var jsonZipItems = await fsUtil.getTables(jsonZip);
+    //var jsonItems = await fsUtil.getTables(json);
+    //var jsonZipItems = await fsUtil.getTables(jsonZip);
 
     res.render('index', { 
         version: pkg.version,
-        csv: csvItems,
+        //csv: csvItems,
         csvZip: csvZipItems,
-        json: jsonItems,
-        jsonZip: jsonZipItems,
+        //json: jsonItems,
+        //jsonZip: jsonZipItems,
         getDate: function (input) {
             return moment(new Date(input)).format("DD.MM.YYYY HH:mm:ss");
         },
@@ -46,7 +46,7 @@ router.get('/', async function(req, res, next) {
 /**
  * Получение информации о таблицах с указанным типом
  * @example
- * /zip 
+ * /csv-zip 
  */
 router.get('/:type', async function(req, res, next) {
     var folder = join(__dirname, '../', 'public', req.params.type);
